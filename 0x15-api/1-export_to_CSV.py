4c08d54ea6f6b3c61bcc364d37ac0739e876aa3f
enzonjagi <amosndungo@gmail.com>
2022-02-01 12:34:30 +0300

    Export to CSV; output format still wrong

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Retrieves info from a RESTAPI using python'''
+import csv
+import requests
+import sys
+
+
+if len(sys.argv) > 1:
+    e_id = sys.argv[1]
+    e_id = int(e_id)
+
+    # request for data
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    # print that info
+    employees = users.json()
+    tasks = todos.json()
+    # find the employee's data
+    for employee in employees:
+        if employee['id'] == e_id:
+            queried_user = employee['name']
+    # tasks the user has completed
+    complete_tasks = 0
+    todo_list = []
+    with open('{}.csv'.format(e_id), 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        for task in tasks:
+            if task["userId"] == e_id:
+                row = ['{}'.format(e_id),
+                       '{}'.format(queried_user),
+                       '{}'.format(task['completed']),
+                       '{}'.format(task['title'])]
+                writer.writerow(row)
